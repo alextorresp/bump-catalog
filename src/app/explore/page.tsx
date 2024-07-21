5ee67b715f2c6d9c228d0e1d29d16e46a78ba687
alextorresp <124532842+alextorresp@users.noreply.github.com>
2024-07-22 01:11:51 +0900

    Add content to explore page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -4,10 +4,17 @@ import Image from 'next/image';
 export default function Explore() {
 
   return (
-    <main className='container centered-column'>
-      <Image src='/explore.png' alt='' width={500} height={150} className='mt-[80px]'/>
-      <h2 className='mt-8 mb-8 text-center'>Search for your favorite albums, songs, or artists</h2>
-      <SearchBar />
+    <main className='container'>
+      <div className='centered-column border border-black border-dashed rounded-2xl py-12 px-4'>
+        <Image src='/explore.png' alt='' width={500} height={150} className=''/>
+        <h2 className='mt-8 mb-8 text-center italic'>Search for your favorite albums, songs, or artists</h2>
+        <SearchBar />
+      </div>
+      <div className='flex flex-col justify-start w-full mt-10 '>
+        <h3 className='font-bold'>Trending Albums</h3>
+        <h3 className='font-bold'>Trending Songs</h3>
+        <h3 className='font-bold'>Trending Artists</h3>
+      </div>
     </main>
   )
 };
