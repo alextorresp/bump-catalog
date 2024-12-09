75b2fa115dc5d4e50da32c2dc02b7acaae416f9f
alextorresp <124532842+alextorresp@users.noreply.github.com>
2024-12-09 01:14:11 -0800

    Slightly refactor CatelogGrid

--- src/components/ui/catalog/CatelogGrid.tsx
+++ src/components/ui/catalog/CatelogGrid.tsx
@@ -10,49 +10,57 @@ type Props= {
 };
 
 export default function CatelogGrid({ title, data }: Props) {
+  const hasData = data.length > 0;
+  const hasMoreThanFiveItems = data.length >= 6;
+
   return (
-    <div className={`relative flex flex-col p-5 border border-black rounded-xl pb-5 overflow-hidden text-white
-    ${data[0] ? 'bg-transparent': 'bg-white text-black border-dashed'}`}
+    <div 
+      className={`relative flex flex-col p-5 border border-black rounded-xl pb-5 overflow-hidden text-white
+      ${hasData ? 'bg-transparent': 'bg-white text-black border-dashed'}`}
     >
-      {data[0] && 
-        <div className='absolute w-full h-full top-0 left-0'>
-          <Image
-          src={data[0].imageSrc}
-          alt={data[0].title}
-          className='top-0 left-0 -z-20 blur-md'
-          fill
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          />
-          <div className='absolute w-full h-full top-0 left-0 -z-10 bg-gradient-to-t from-40% from-black to-transparent'></div>
-        </div>
-      }
+      {
+        hasData && (
+          <div className='absolute w-full h-full top-0 left-0'>
+            <Image
+              src={data[0].imageSrc}
+              alt={data[0].title}
+              className='top-0 left-0 -z-20 blur-md'
+              fill
+              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            />
+            <div className='absolute w-full h-full top-0 left-0 -z-10 bg-gradient-to-t from-40% from-black to-transparent'></div>
+          </div>
+      )}
 
       <h3 className='font-bold mb-6 z-10 text-center bg-black rounded-md py-3 '>{title}</h3>
-      <div className={`
-        ${!data[0] ? 'hidden' : 'grid'} 
-        grid-cols-1 w-full place-items-center`}
-        >
-        
-        <div className='md:col-span-1 grid-rows-1 gap-4 md:w-[30%] sm:w-[65%] w-[65%] md:mb-10 mt-6'>
-          {data[0] && (
+
+      {/* Grid displaying list */}
+      <div className={`${!hasData ? 'hidden' : 'grid'} grid-cols-1 w-full place-items-center`}>
+        {/* Item #1 */}
+        <div 
+          className='md:col-span-1 grid-rows-1 gap-4 md:w-[30%] sm:w-[65%] w-[65%] md:mb-10 mt-6'>
+          {hasData && (
             <CatelogCard position={1} itemData={data[0]} />
           )}
         </div>
 
-        
-        <div className={`
-          ${!data[5] ? 'md:grid-rows-1' : 'md:grid-rows-2'} 
+        {/* Items #2-10 */}
+        <div 
+          className={`
+          ${!hasMoreThanFiveItems ? 'md:grid-rows-1' : 'md:grid-rows-2'} 
           ${!data[1] ? 'hidden' : 'grid'}
           md:gap-2 lg:gap-3 w-full mb-3 place-items-center`}
-          >
+        >
+          {/* Items #2-6 */}
           <div className='row-1 grid md:grid-cols-5 grid-cols-1 p-7 md:gap-[15px] lg:gap-[30px] md:border border-gray-400 rounded-xl border-dashed md:w-full sm:w-[70%] w-[70%]'>
             {data.slice(1, 6).map((item, index) => (
               <CatelogCard position={index + 2} itemData={item} key={index}/>
             ))}
           </div>
-
+          
+          {/* Items 7-10 */}
           <div className={`row-2
-            ${!data[5] ? 'hidden' : 'grid '} 
+            ${!hasMoreThanFiveItems ? 'hidden' : 'grid '} 
             md:grid-cols-5 grid-cols-1 gap-4 md:border border-gray-400 p-7 rounded-xl border-dashed md:w-full sm:w-[70%] w-[70%]`}
             >
             {data.slice(6, 11).map((item, index) => (
@@ -62,21 +70,19 @@ export default function CatelogGrid({ title, data }: Props) {
         </div>
       </div>
 
-      {
-        data.length === 0 && <p className='text-black text-center'>No items. Go explore and add some!</p>
-      }
+      {!hasData && <p className='text-black text-center'>No items. Go explore and add some!</p>}
 
       <div className='flex flex-row justify-center gap-4 mt-3 mb-2'>
         {data.length < 10 && 
-          <ButtonOne text='Add items!' link='/explore' className={`
-            ${data[0] ? 'hover:bg-slate-300 hover:text-black text-black bg-white': 'bg-black text-white hover:bg-slate-300 hover:text-black'}
+          (<ButtonOne text='Add items!' link='/explore' className={`
+            ${hasData ? 'hover:bg-slate-300 hover:text-black text-black bg-white': 'bg-black text-white hover:bg-slate-300 hover:text-black'}
             border-none z-20  w-[150px] text-center`}
           />
-        }
+        )}
 
       {data.length >= 2 &&
-        <ButtonOne text='Reorder Items' className='hover:bg-slate-300 bg-white border-none z-20 text-black hover:text-black w-[150px]'/>
-      }
+        (<ButtonOne text='Reorder Items' className='hover:bg-slate-300 bg-white border-none z-20 text-black hover:text-black w-[150px]'/>
+        )}
       </div>
   </div>
 )
